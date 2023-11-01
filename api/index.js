a75cdd4abceaaa589dbbf71b9863535a9d38faa1
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-11-01 12:00:39 -0400

    create API test route

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ const app = express();
 import colors from 'colors'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js';
 dotenv.config()
+
 
 mongoose.connect(process.env.DATABASE_URL)
 
@@ -13,7 +15,8 @@ db.on('connected', () => console.log('DATABASE is connected...'))
 db.on('disconnected', () => console.log('DB is disconnected'))
 db.on('error', (err) => console.log(err.message + ' is DATABASE configured correctly?'))
 
-
+//Routes...
+app.use('/api/v1', userRouter)
 
 
 app.listen(3000, () => {
