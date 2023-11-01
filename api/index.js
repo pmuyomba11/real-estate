7deb95db4acbcf5a2804f276ed28b83162d662b5
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2023-11-01 11:17:05 -0400

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
-import colors from 'colors'
-
 const app = express();
+import colors from 'colors'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.DATABASE_URL)
+
+const db = mongoose.connection;
+
+db.on('connected', () => console.log('DATABASE is connected...'))
+db.on('disconnected', () => console.log('DB is disconnected'))
+db.on('error', (err) => console.log(err.message + ' is DATABASE configured correctly?'))
+
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on PORT 3000...'.inverse.bold.blue);
